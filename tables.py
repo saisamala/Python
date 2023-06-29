e64664d61c5978b69e563e4be0fb18351557f66b
saisamala <sai.samala22@gmail.com>
2023-06-29 09:26:21 +0530

    added tables.py file

---
+++ tables.py
@@ -0,0 +1,9 @@
+num = int(input("Enter your number: "))
+
+# for i in range(1,11):
+#     print(f"{num} * {i} = "+ str(num*i))
+
+i=1
+while(i<=10):
+    print(f"{num} * {i} = "+ str(num*i))
+    i+=1
